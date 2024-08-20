9da776799da8e1d97f9d2b1c7f1d57b8a7128eed
dhavalvadher <dhavalvadher0001@gmail.com>
2024-08-20 10:27:21 +0530

    server

--- src/index.js
+++ src/index.js
@@ -6,17 +6,43 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const { facebookProvider, googleProvider } = require('./utils/Provider');
-const connectChat = require('./utils/socketIO');
+// const connectChat = require('./utils/socketIO');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./src/api.yaml');
+// const swaggerDocument = YAML.load('./src/api.yaml');
+const path = require('path')
 
 const app = express()
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(cors({
-  origin:'http://localhost:3000',
-  credentials: true
-}))
+
+googleProvider();
+facebookProvider();
+
+const _dirname = path.resolve();
+
+const __swaggerDistPath = path.join(_dirname, 'node_modules', 'swagger-ui-dist'); //install swagger-ui-dist
+
+const swaggerDocument = YAML.load(path.resolve('./public', 'api.yaml'));
+
+
+app.use(
+  '/api/docs',
+  express.static(__swaggerDistPath, { index: false }), // Serve Swagger UI assets
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: {
+      url: '/public/api.yaml' // Path to your YAML file
+    }
+  })
+);
+
+
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use(cors({
+//   origin:'http://localhost:3000',
+//   credentials: true
+// }))
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(require('express-session')({
@@ -30,10 +56,14 @@ app.use(passport.session());
 
 
 connectDB();
-googleProvider();
-facebookProvider();
-connectChat();
+
+
+// connectChat();
 app.use('/api/v1', Routes);
+
+app.get("/", (req,res) => {
+  res.send("Hello, World!");
+})
 
 app.listen(9000, () => {
   console.log("Server started at port 9000.");
