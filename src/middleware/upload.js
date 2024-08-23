80a726fa81023ff22f19878c6ea7a1d88ebc3888
dhavalvadher <dhavalvadher0001@gmail.com>
2024-08-23 10:17:21 +0530

    dd

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -6,21 +6,21 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
 
         console.log(file.fieldname);
+        let fieldname = req.fieldname
+        // const filepath = path.join('public', fieldname)
 
-        // const filepath = path.join('public', file.fieldname)
+        const filepath = path.join("/tmp", fieldname)
+        console.log("filepathhhhh", filepath);
 
-        const filepath = path.join("/tmp",fieldname)
-        console.log("filepathhhhh",filepath);
-        
-        
+
         fs.mkdir(filepath, {
             recursive: true
         }, (err) => {
             if (err) {
-               return cb(err,null)
+                return cb(err, null)
             }
         })
-        cb(null,filepath)
+        cb(null, filepath)
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
